c2b6907da0d90bb311f068974863905c71150774
FanZhenzhen <fzz@qq.com>
2016-09-29 15:57:52 +0800

    服务端处理post请求，保存数据到数据库

--- index.js
+++ index.js
@@ -2,6 +2,24 @@ var exp = require('express')
 var app = exp()
 app.use(exp.static('static'))
 
+var bodyParser = require('body-parser')
+app.use(bodyParser.json())
+
+// 导入数据库模块
+var Task = require('./db.js')
+
+// 保存数据到数据库
+app.post('/task/:date',(req,res)=>{
+    var task = new Task(req.body)
+    task.save(function(error){
+        if(error){
+            res.json({'result':0})
+        }else{
+            res.json({'result':1})
+        }
+    })
+})
+
 app.listen(3000,()=>{
     console.log('服务器监听3000端口')
 })
