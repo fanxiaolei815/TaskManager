2465304bccaca78d58be74817a50547f88ccae43
FanZhenzhen <fzz@qq.com>
2016-09-29 16:37:16 +0800

    采用模板化将数据展示到页面上

--- static/js/views/list.js
+++ static/js/views/list.js
@@ -1,6 +1,6 @@
 define([
-    'backbone'
-], function(B) {
+    'backbone','template'
+], function(B,T) {
     return B.View.extend({
         el:'section',
         events:{
@@ -10,11 +10,26 @@ define([
             // 声明变量赋值为this
             var self = this 
            //从服务端获取当前日期对应的数据展示
-           this.model.fetch().done(function(data){
+           this.model.fetch().done(function(){
             //    这里，this不是视图类对象
                console.log(self.model)
-           })
+                
+                //    展示数据到页面
 
+                // 组织数据为数据
+                var arr = self.model.models.map(function(item,index,array){
+                    return {
+                        id:item.get('id'),
+                        content:item.get('content'),
+                        time:item.get('time')
+                    }
+                })
+                // 采用artTemplate将模板和数据结合
+                var html = T('listTemplate',{arr})
+                //  填充到页面上
+                self.$('ul').html(html)
+                console.log(html)
+           })
         }
     })
     
