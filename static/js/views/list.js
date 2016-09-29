1ea75d30a5f78168f2b388989a79368098fc3cce
FanZhenzhen <fzz@qq.com>
2016-09-29 16:01:44 +0800

    新建list.js视图类，与section标签关联

---
+++ static/js/views/list.js
@@ -0,0 +1,15 @@
+define([
+    'backbone'
+], function(B) {
+    return B.View.extend({
+        el:'section',
+        events:{
+
+        },
+        initialize:function(){
+           
+
+        }
+    })
+    
+});
--- static/main.js
+++ static/main.js
@@ -13,7 +13,8 @@ require.config({
         'jquery-ui':['jquery']
     }
 })
-require(['jquery','js/views/header','js/views/footer','js/collections/taskset'],function($,Header,Footer,Taskset){
+require(['jquery','js/views/header','js/views/footer',
+'js/collections/taskset','js/views/list'],function($,Header,Footer,Taskset,List){
     // 页面加载完成，风火轮消失，主界面展示出现
     $(function(){
         $('#loading').hide()
@@ -24,4 +25,5 @@ require(['jquery','js/views/header','js/views/footer','js/collections/taskset'],
     // 将header视图与集合关联；在header中修改的日期需要通过集合传递到footer中
     new Header({model:taskset})
     new Footer({model:taskset})
+    new List({model:taskset})
 })
